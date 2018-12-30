8e0f117338482c7070eed6dfc36b6fde2e65f0bd
yashn26 <33773056+yashn26@users.noreply.github.com>
2018-12-30 22:51:21 +0530

    Add files via upload

---
+++ Getter.ino
@@ -0,0 +1,110 @@
+const byte clkPin = 5;
+const byte dataPin = 2;
+const int shift = 1;
+boolean adcStarted;
+volatile int adcReading;
+volatile boolean adcDone;
+volatile boolean clk;
+volatile int wordcount = 0;
+volatile byte worddata = B00000001;
+char letter;
+
+
+
+
+void ADC_Init() {
+  ADCSRA = bit(ADEN);     //turn ADC on
+  ADCSRA |= bit (ADPS0) |  bit (ADPS1) | bit (ADPS2);  // Prescaler of 128
+  ADMUX  =  bit (REFS0) | bit (REFS1);    // internal 1.1v reference
+}
+
+void ADC_start_conversion(int adc_pin) {
+  ADMUX &= ~(0x07) ; //clearing enabled channels
+  ADMUX  |= (adc_pin & 0x07) ;    // select input port
+  bitSet (ADCSRA, ADSC) ;  //start ADC read
+}
+
+int ADC_read_conversion() {
+  while (bit_is_set(ADCSRA, ADSC)); //checking if pin is reading mode
+  return ADC ;
+}
+
+void getData() {
+  ADC_start_conversion(dataPin);
+  int dataValue = ADC_read_conversion();
+  //Serial.println(dataValue);
+  if (dataValue > 500) {
+    worddata <<= shift; //left shift once
+    worddata |= B00000001; //put 1 at LSB
+
+    ADC_start_conversion(clkPin); //start back the clock reading
+  }
+  else {
+    worddata <<= shift; //left shift once
+    ADC_start_conversion(clkPin); //start back the clock reading
+  }
+}
+
+void setup ()
+{
+  Serial.begin (115200);
+  ADC_Init();
+  ADC_start_conversion(clkPin);
+  Serial.write("Ready To Recieve...\n");
+
+}  // end of setup
+
+
+// ADC complete ISR
+ISR (ADC_vect) //dont think we need this can replace the interupt with readconversion method
+{
+  byte low, high;
+
+  // we have to read ADCL first; doing so locks both ADCL
+  // and ADCH until ADCH is read.  reading ADCL second would
+  // cause the results of each conversion to be discarded,
+  // as ADCL and ADCH would be locked when it completed.
+  low = ADCL;
+  high = ADCH;
+
+  adcReading = (high << 8) | low;
+  adcDone = true;
+}  // end of ADC_vect
+
+void loop () {
+  if (adcDone) //ADC complete interrupt keeps reading the value if its done then we check if the reading is high or low
+  {
+    //Serial.println(adcReading);
+    adcStarted = false; //stop reading
+    if (adcReading > 300 && clk == false) { //clock HIGH reading is done here
+      //Serial.println("clk 1");
+      getData();
+      clk = true;
+      wordcount++;
+      //Serial.println(worddata, BIN);
+    }
+    if (adcReading < 300 && clk == true) { //clock LOW reading is done here
+      //Serial.println("clk 0");
+      getData();
+      clk = false;
+      wordcount++;
+      //Serial.println(worddata, BIN);
+    }
+    // print the reading after 8 iterations (1 byte at a time)
+    if (wordcount == 8) {
+      letter = worddata;
+      Serial.write(letter);
+      wordcount = 0;
+    }
+
+    adcDone = false;
+  }
+
+  // if we aren't taking a reading, start a new one
+  if (!adcStarted)
+  {
+    adcStarted = true;
+    // start the conversion
+    ADCSRA |= bit (ADSC) | bit (ADIE);
+  }
+}  // end of loop
---
+++ Sender.ino
@@ -0,0 +1,76 @@
+int ledState = LOW;// ledState used to set the LED
+const int clkPin =  13;// the number of the LED pin for clock
+const int dataPin = 11;//The number of the LED pin for data
+const long interval = 100;// interval at which to blink (milliseconds)
+unsigned long previousMillis = 0;        // will store last time LED was updated
+volatile char dataword; //The single character
+volatile int i = 7; //index for the dataword 8bits
+volatile int j = 0;
+
+bool readserial() {
+  if (Serial.available()) { //constructing the data frame only if serial is ready
+    dataword = Serial.read();
+     
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void moddata() { //modulate the data HIGH = 1 , LOW = 0
+  if (bitRead(dataword, i) == 1) {
+    digitalWrite(dataPin, HIGH);
+    i--;
+  }
+  else if (bitRead(dataword, i) == 0) {
+    digitalWrite(dataPin, LOW);
+    i--;
+  }
+  if (i == -1) {
+    i = 7;
+  }
+}
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(clkPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  Serial.begin (115200);
+  Serial.write("Sender Ready For Input...\n");
+}
+
+
+
+void loop() {
+  // check to see if it's time to blink the LED; that is, if the difference
+  // between the current time and last time you blinked the LED is bigger than
+  // the interval at which you want to blink the LED.
+  j = 0;
+  bool serialready = readserial();
+  if (serialready) { //start the clock and hence transmission only if data is ready
+    while (j < 8) { //send the byte(8bits) then reset for next byte
+
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis; // save the last time you blinked the LED
+        if (ledState == LOW) {
+          ledState = HIGH;
+          digitalWrite(clkPin, HIGH);
+          moddata();
+          j++;
+          
+        }
+        else {
+          digitalWrite(clkPin, LOW);
+          ledState = LOW;
+          moddata();
+          j++;
+        }
+        
+      }
+    }
+    Serial.write("Sending - ");
+        Serial.println(dataword);
+  }
+}
